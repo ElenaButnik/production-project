e6c91139d481937c451f3e7a2fca1eb4ce864289
ElenaButnik <73306047+ElenaButnik@users.noreply.github.com>
2023-02-15 20:08:46 +0200

    improve routing

---
+++ src/app/App.tsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import { classNames } from "shared/lib/classNames/classNames";
+import { useTheme } from "app/providers/ThemeProvider";
+import { AppRouter } from "./providers/router";
+import "./styles/index.scss";
+
+const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <div className={classNames("app", {}, [theme])}>
+      <button onClick={toggleTheme}>Toggle</button>
+      <Link to={"/"}>MainPage</Link>
+      <Link to={"/about"}>About</Link>
+      <AppRouter />
+    </div>
+  );
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,13 @@
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import App from './App';
-import ThemeProvider from './theme/ThemeProvider';
+import { ThemeProvider } from "app/providers/ThemeProvider";
+import App from "./app/App";
 
 ReactDOM.render(
-    <BrowserRouter>
+  <BrowserRouter>
     <ThemeProvider>
-     <App />
-     </ThemeProvider>
-     </BrowserRouter>
-,
-    document.getElementById("root")
-)
-
+      <App />
+    </ThemeProvider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
