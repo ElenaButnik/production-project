b4a9787437bf7ba5083b358c78c6541b48c1e8f7
ElenaButnik <butnik.elena@gmail.com>
2023-02-19 21:19:54 +0200

    add button functional component

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,6 @@
-import { Link } from "react-router-dom";
 import { classNames } from "shared/lib/classNames/classNames";
 import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
+import { ThemeSwitcher } from "widgets/ThemeSwitcher";
 import cls from "./Navbar.module.scss";
 
 interface NavbarProps {
@@ -9,6 +9,7 @@ interface NavbarProps {
 export const Navbar = ({ className }: NavbarProps) => {
   return (
     <div className={classNames(cls.navbar, {}, [className])}>
+      <ThemeSwitcher />
       <ul className={cls.links}>
         <li>
           <AppLink theme={AppLinkTheme.SECONDARY} to={"/"}>
